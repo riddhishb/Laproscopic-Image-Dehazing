a10469c177603d711a6ac2bbac49e229623ee6ef
riddhishb <riddhishb@gmail.com>
2015-05-05 22:43:55 +0530

    Matlab code with 1fps

--- Haze-Removal/Project/run.m
+++ Haze-Removal/Project/run.m
@@ -3,7 +3,7 @@
  tic;
 [a,b,c,d,e] = removeHaze('10.png',21);
 toc
-imwrite(a,'Results/10-in.png');
-imwrite(b,'Results/10-out.png');
+% imwrite(a,'Results/10-in.png');
+% imwrite(b,'Results/10-out.png');
 
 % toc;
