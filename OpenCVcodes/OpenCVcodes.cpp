1671f0b8e9169bc6cf128c7c3b86a70894014551
riddhishb <riddhishb@gmail.com>
2015-06-12 17:22:15 +0530

    Added video feature

--- OpenCVcodes/OpenCVcodes.cpp
+++ OpenCVcodes/OpenCVcodes.cpp
@@ -25,6 +25,7 @@ double maxVal1,maxVal2,maxVal3;
 Mat ROIr,ROIb,ROIg;
 int array_for_min[2] = {0,0};
 
+
 Mat makeDarkChannel(Mat I,int patch_size){
 
      Mat J = Mat::zeros(I.size(),I.type());
@@ -131,39 +132,8 @@ Mat estimateA(Mat I, Mat J, int numPixels){
     cv::addWeighted(im, 1.5, tmp, -0.5, 0, im);
 }
 
-int main(){
-
- Mat input;
-double arielPerspective = 0.95;
-
- input = imread("10.png");
-
-// imshow("Input",input);
-    resize(input, input, Size(), 0.25, 0.25, INTER_LINEAR);
-    //input.convertTo(input,CV_32FC3,1/255.0);
-
-   
-    Mat dark_ch = makeDarkChannel(input,17);
-
-    int numBrightestPixels = cvCeil(0.001 * dark_ch.rows * dark_ch.cols); // Use the cieling to overestimate number needed
-      
-    Mat A = estimateA(input,dark_ch,numBrightestPixels);
-    Mat tx_map;
-    Mat out;
-
-    divide(input,A,out,255);
-
-    tx_map = 255 - arielPerspective*makeDarkChannel(out,17);
-    
-    // Mat tx_out,input_gray;
-    // cvtColor(input,input_gray,CV_BGR2GRAY);
-    // tx_out = tx_map;//
-    // tx_out = guidedFilter(input_gray, tx_map, 3, 1e-6);
-   
-
-
-    // Now to reconstruct the Dehazed image
-    Mat dehazed;
+Mat getDehazed(Mat input,Mat A,Mat tx_map){
+Mat dehazed;
     dehazed = Mat::zeros(input.size(),input.type());
 
     for(int i = 0;i<dehazed.rows;i++){
@@ -182,90 +152,151 @@ double arielPerspective = 0.95;
         }
     }
 
+    return dehazed;
+}
+
+// int main(){
+
+//  Mat input;
+// double arielPerspective = 0.95;
+
+//  input = imread("10.png");
+
+// // imshow("Input",input);
+//     resize(input, input, Size(), 0.25, 0.25, INTER_LINEAR);
+//     //input.convertTo(input,CV_32FC3,1/255.0);
+
+   
+//     Mat dark_ch = makeDarkChannel(input,17);
+//     // imshow("dark_ch",dark_ch);
+//     int numBrightestPixels = cvCeil(0.001 * dark_ch.rows * dark_ch.cols); // Use the cieling to overestimate number needed
+      
+//     Mat A = estimateA(input,dark_ch,numBrightestPixels);
+//     Mat tx_map;
+//     Mat out;
+
+//     divide(input,A,out,255);
+
+//     tx_map = 255 - arielPerspective*makeDarkChannel(out,17);
+    
+//     // Mat tx_out,input_gray;
+//     // cvtColor(input,input_gray,CV_BGR2GRAY);
+//     // tx_out = tx_map;//
+//     // tx_out = guidedFilter(input_gray, tx_map, 3, 1e-6);
+   
+//     // Now to reconstruct the Dehazed image
+    
+//     Mat dehazed = getDehazed(input,A,tx_map);
+    
+//      // cout << tx_out;
+     
+     
+//      resize(dehazed, dehazed, Size(), 4, 4, INTER_LINEAR);
+//      unsharpMask(dehazed);
+//      unsharpMask(dehazed);
+//      // unsharpMask(dehazed);
+    
+//      t = clock();
+//      imshow("Final ",dehazed);
+//      imwrite("../out.png",dehazed);
+//      cout << ((float)t)/CLOCKS_PER_SEC << "seconds" << endl;
+//      waitKey(0);
+//      return 0;
+// }
+
+
+ 
+   int main()
+{
+    double arielPerspective = 0.95;
+    const string source      = "2.mp4";           // the source file name
+    const bool askOutputType =true;  // If false it will use the inputs codec type
+
+    VideoCapture inputVideo(source);              // Open input
+    if (!inputVideo.isOpened())
+    {
+        cout  << "Could not open the input video: " << source << endl;
+        return -1;
+    }
+
+    string::size_type pAt = source.find_last_of('.');                  // Find extension point
+    const string NAME = "output2.avi";   // Form the new name with container
+    int ex = static_cast<int>(inputVideo.get(CV_CAP_PROP_FOURCC));     // Get Codec Type- Int form
+
+    // Transform from int to char via Bitwise operators
+    char EXT[] = {(char)(ex & 0XFF) , (char)((ex & 0XFF00) >> 8),(char)((ex & 0XFF0000) >> 16),(char)((ex & 0XFF000000) >> 24), 0};
+
+    Size S = Size((int) inputVideo.get(CV_CAP_PROP_FRAME_WIDTH),    // Acquire input size
+                  (int) inputVideo.get(CV_CAP_PROP_FRAME_HEIGHT));
+
+
+    VideoWriter outputVideo(NAME,CV_FOURCC('P','I','M','1'),inputVideo.get(CV_CAP_PROP_FPS),S,true);                                        // Open the output
+    // if (askOutputType)
+    //     outputVideo.open(NAME, ex=-1, inputVideo.get(CV_CAP_PROP_FPS), S, true);
+    // else
+    //     outputVideo.open(NAME, ex, inputVideo.get(CV_CAP_PROP_FPS), S, true);
+
+    // if (!outputVideo.isOpened())
+    // {
+    //     cout  << "Could not open the output video for write: " << source << endl;
+    //     return -1;
+    // }
+
+    cout << "Input frame resolution: Width=" << S.width << "  Height=" << S.height
+         << " of nr#: " << inputVideo.get(CV_CAP_PROP_FRAME_COUNT) 
+         << "FPS: " << inputVideo.get(CV_CAP_PROP_FPS) <<endl;
+    cout << "Input codec type: " << EXT << endl;
+
+    int channel = 2; // Select the channel to save
+    int alpha = 0.8;
+    Mat frame;
+    int count = 0;
+    for(;;) //Show the image captured in the window and repeat
+    {   
+
+        inputVideo >> frame;              // read
+        if (frame.empty()) break;         // check if at end
+         resize(frame, frame, Size(), 0.25, 0.25, INTER_LINEAR);
+
+       Mat dark_ch = makeDarkChannel(frame,17);
+    // imshow("dark_ch",dark_ch);
+    int numBrightestPixels = cvCeil(0.001 * dark_ch.rows * dark_ch.cols); // Use the cieling to overestimate number needed
+    Mat A_prev;  
+    // if(count != 0){Mat A_prev = A;}  
+    Mat A = estimateA(frame,dark_ch,numBrightestPixels);
+
+    
+     if(count != 0){
+        // Do alpha blending
+        addWeighted(A,alpha,A_prev,1-alpha,0.0,A);
+     }
+
+    Mat tx_map;
+    Mat out;
+
+    divide(frame,A,out,255);
+
+    tx_map = 255 - arielPerspective*makeDarkChannel(out,17);
+   
+    // Now to reconstruct the Dehazed image
+    
+    Mat dehazed = getDehazed(frame,A,tx_map);
     
      // cout << tx_out;
-     
+     // SUGGESTION : Have an alpha blending between A, so the sharp changes are averted
+     // on all therre channels
+
      
      resize(dehazed, dehazed, Size(), 4, 4, INTER_LINEAR);
      unsharpMask(dehazed);
      unsharpMask(dehazed);
-     // unsharpMask(dehazed);
-    
+       //outputVideo.write(res); //save or
+     A_prev = A;
+       // outputVideo << dehazed;
+     outputVideo.write(dehazed);
+    }
 
-     imshow("Final ",dehazed);
-     imwrite("../out.png",dehazed);
-     cout << ((float)t)/CLOCKS_PER_SEC << "seconds" << endl;
-     waitKey(0);
-     return 0;
+    cout << "Finished writing" << endl;
+    return 0;
 }
-
-
-// int main()
-// {
-    // //for video defogging
     
-    // // VideoCapture vid("your video file");
-    // // if(!vid.isOpened())
-    // //     return -1;
-    // // double rate = vid.get(CV_CAP_PROP_FPS);
-    // // int delay = 1000/rate;
-    // //     bool stop(false);
-
-    // Mat frame;
-    // frame = imread("10.png");
-    // Mat darkChannel;
-    // Mat T;
-    // Mat fogfree;
-    // double alpha = 0.05;    //alpha smoothing
-    // int Airlightp;          //airlight value of previous frame
-    // int Airlight;           //airlight value of current frame
-    // int FrameCount = 0;     //frame number
-    //     int ad;                 //temp airlight value
-    //     namedWindow("before and after", CV_WINDOW_AUTOSIZE);
-    
-    // // for(;;)
-    // // {
-    // //     vid >> frame;
-    // //     FrameCount++;
-    // //     if(vid.get(CV_CAP_PROP_POS_AVI_RATIO)==1)
-    // //         break;
-
-    //     //create mat for showing the frame before and after processing
-    //         Mat beforeafter = Mat::zeros(frame.rows, 2 * frame.cols, CV_8UC3);
-    //         Rect roil (0, 0, frame.cols, frame.rows);
-    //         Rect roir (frame.cols, 0, frame.cols, frame.rows);
-
-    //     //first frame, without airlight smoothing
-    //     if (FrameCount == 1)
-    //     {
-    //         darkChannel = getMedianDarkChannel(frame, 5);
-    //             Airlight = estimateA(darkChannel);
-    //             T = estimateTransmission(darkChannel, Airlight);
-    //         ad = Airlight;
-    //                     fogfree = getDehazed(frame, T, Airlight);
-    //     }
-
-    //     //other frames, with airlight smoothing
-    //     // else
-    //     // {
-    //     //     double t = (double)getTickCount();
-
-    //     //     Airlightp = ad;
-    //     //     darkChannel = getMedianDarkChannel(frame, 5);
-    //     //         Airlight = estimateA(darkChannel);
-    //     //         T = estimateTransmission(darkChannel, Airlight);
-    //     //                 cout<<"previous:"<<Airlightp<<"--current:"<<Airlight<<endl;
-    //     //         ad = int(alpha * double(Airlight) + (1 - alpha) * double(Airlightp));//airlight smoothing
-    //     //         cout<<"smoothed airlight is:"<<ad<<endl;
-    //     //         fogfree = getDehazed(frame, T, ad);
-
-    //     //     t = (double)cvGetTickCount() - t;
-    //     //     printf( "=============Execution time per frame = %gms\n", t/((double)cvGetTickFrequency()*1000.) );
-    //     // }
-    //         frame.copyTo(beforeafter(roil));
-    //         fogfree.copyTo(beforeafter(roir));
-    //     imshow("before and after", beforeafter);
-
-    // //     if(waitKey(delay) >= 0)
-    // //         stop = true;
-    // // // }
